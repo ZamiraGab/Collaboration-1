d076091f364e4d822f6c41507be18bbce77bedf4
Zamira <zamira.gab07@icloud.com>
2023-08-15 12:59:53 -0400

    Initial Commit

---
+++ Collaboration 1/Collaboration_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Collaboration_1App.swift
+//  Collaboration 1
+//
+//  Created by Zamira Gabdyzhamalova on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct Collaboration_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
