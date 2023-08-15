9155dcd1fbbf7676e0e808f273d8f6683b827999
Zamira <zamira.gab07@icloud.com>
2023-08-15 13:09:38 -0400

    added content

--- Collaboration 1/ContentView.swift
+++ Collaboration 1/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("Happy to be coding with you")
         }
         .padding()
     }
